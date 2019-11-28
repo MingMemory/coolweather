dc7b63fe7021b0762c855ad7d893d852f5602bd9
ftm <ftm@admin.net>
2019-11-28 15:29:00 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.coolweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //缓冲数据的判断——一开始从SharedPreferences文件中读取缓冲数据，如果不为null，说明之前已经请求过天气数据，直接跳转WeatherActivity
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getString("weather",null) != null){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
